43a4a35f84271f2b6e59b063f5ef2f3439b79f17
Marek121212 <marek.coronic@centrum.sk>
2024-10-08 08:22:45 +0200

    druha zmena

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import sk.upjs.jpaz2.ObjectInspector;
+import sk.upjs.jpaz2.Turtle;
+import sk.upjs.jpaz2.WinPane;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WinPane plocha = new WinPane(500,500);
+        MojaKorytnacka jozko = new MojaKorytnacka();
+        plocha.add(jozko);
+        jozko.kriz(100);
+
     }
 }
